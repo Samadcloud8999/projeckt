1edf46a55ef8ee295540675ba2755201ed07a30f
Doni <ruslanovdoni05@gmail.com>
2025-04-23 09:36:27 +0600

    header cmpnt

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./Header.module.css";
+import logo from "../assets/logo.svg"; // Замените на свой логотип
 
-export const Header = () => {
+function Header() {
   return (
-    <div>
-        <h1>Hedaer</h1>
-    </div>
-  )
+    <header className="header-top">
+      <div className="header-top__container">
+        <div className="logo">
+          <img src={logo} alt="Логотип" />
+        </div>
+        <nav className="nav">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <a href="#">Главная</a>
+            </li>
+            <li className="nav__item">
+              <a href="#">О нас</a>
+            </li>
+            <li className="nav__item">
+              <a href="#">Услуги</a>
+            </li>
+            <li className="nav__item">
+              <a href="#">Контакты</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="header-actions">
+          <button className="btn btn-primary">Связаться</button>
+        </div>
+      </div>
+    </header>
+  );
 }
+
+export default Header;
